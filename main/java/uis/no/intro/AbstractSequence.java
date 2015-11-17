4f27e801d82a7653382f788c95c54e18bf556993
Erlend Tøssebro <erlend.tossebro@uis.no>
2015-11-17 14:54:37 +0100

    searchablesequence

--- main/java/uis/no/intro/AbstractSequence.java
+++ main/java/uis/no/intro/AbstractSequence.java
@@ -1,6 +1,6 @@
 package main.java.uis.no.intro;
 
-public abstract class AbstractSequence implements Sequence {
+public abstract class AbstractSequence implements SearchableSequence {
 	public int sum(int n) {
 		int result = 0;
 		for (int i=0;i<n;i++) {
@@ -16,5 +16,23 @@ public abstract class AbstractSequence implements Sequence {
 			result *= getElement(i);
 		}
 		return result;
+	}
+	
+	public int findElementIn(int k, int limit) {
+		int lowestindex = 0;
+		int highestindex = limit;
+		int currentindex;
+		int currentValue = getElement(highestindex);
+		if (currentValue < k) return -1;
+		if (currentValue == k) return highestindex;
+		currentindex = highestindex/2;
+		while (highestindex-lowestindex > 1) {
+			currentValue = getElement(currentindex);
+			if (currentValue == k) return currentindex;
+			if (currentValue < k) lowestindex = currentindex;
+			if (currentValue > k) highestindex = currentindex;
+			currentindex = (lowestindex + highestindex)/2;
+		}
+		return -1;
 	}
 }
